b969196bb27e7e457e3267588efd0666b00b2efd
Shantanu Sardal <ufdeveloper@gmail.com>
2022-08-07 22:49:08 -0400

    more changes

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,18 +1,38 @@
 import classes from './StartingPageContent.module.css';
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import "react-datepicker/dist/react-datepicker.css";
 import React, { useEffect, useContext, useState } from 'react';
 import { Header, Icon, Message, Table } from 'semantic-ui-react';
 import { LineChart, Line, Tooltip, CartesianGrid, XAxis, YAxis, Legend, ResponsiveContainer } from 'recharts';
+import DatePicker from "react-datepicker";
 
 const StartingPageContent = () => {
 
     const [activities, setActivities] = useState(null);
     const [activitiesFetchFailed, setActivitiesFetchFailed] = useState(false);
+    const datePrevMonth = new Date().setMonth(new Date().getMonth() - 1);
+    const [startDate, setStartDate] = useState(new Date(datePrevMonth));
+    const [endDate, setEndDate] = useState(new Date());
+
+    const startDatePicker = <DatePicker selected={startDate} onChange={(date) => {
+        setStartDate(date);
+        console.log('startDate=' + date);
+    }} />
+    const endDatePicker = <DatePicker selected={endDate} onChange={(date) => {
+        setEndDate(date);
+        console.log('endDate=' + date);
+    }} />
+
+    const datePicker = <div>
+        Start Date {activities && startDatePicker}
+        End Date {activities && endDatePicker}
+    </div>
 
     const domain = ['auto', 'auto'];
 
     const renderLineChart = (
         // <ResponsiveContainer width="50%" height="50%">
-            <LineChart width={600} height={600} data={activities}>
+            <LineChart width={800} height={600} data={activities}>
                 <Line type="monotone" dataKey="heartRate" stroke="#8884d8" />
                 <Line type="monotone" dataKey="weight" stroke="#ff000f" />
                 <Line type="monotone" dataKey="elevationGain" stroke="#204b71" />
@@ -28,7 +48,11 @@ const StartingPageContent = () => {
 
     useEffect(() => {
         // Fetch activities
-        fetch('http://localhost:8080/api/v1/activities', {
+        console.log("fromDate=" + startDate.toISOString() + ", toDate=" + endDate.toISOString());
+        fetch('http://localhost:8080/api/v1/activities' +
+                    '?fromDate=' + startDate.toISOString() +
+                    '&toDate=' + endDate.toISOString() +
+                    '&activityType=Run', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -60,44 +84,18 @@ const StartingPageContent = () => {
                 setActivitiesFetchFailed(true);
                 alert(err.message);
             });
-    }, []);
+    }, [startDate, endDate]);
   return (
-      <div>
-          <Header as="h1">
-              <Icon name="credit card outline" />
-              My Activities
-          </Header>
+      <div style={{align: 'center'}}>
+          <Header as="h1">My Activities</Header>
+
           {activitiesFetchFailed && <Message error header="Failed to fetch activities." />}
 
           {!activities && !activitiesFetchFailed && <p>Charting your activity data..</p>}
 
-          {renderLineChart}
+          {activities && datePicker}
 
-          {/*{activities*/}
-          {/*&& (*/}
-          {/*    <div>*/}
-          {/*        <Table>*/}
-          {/*            <thead>*/}
-          {/*            <tr>*/}
-          {/*                <th>Activity Id</th>*/}
-          {/*                <th>Heart Rate</th>*/}
-          {/*                <th>Weight</th>*/}
-          {/*            </tr>*/}
-          {/*            </thead>*/}
-          {/*            <tbody>*/}
-          {/*            {activities.map((activity) => (*/}
-          {/*                <tr id={activity.id} key={activity.id}>*/}
-          {/*                    <td>{activity.id}</td>*/}
-          {/*                    <td>{activity.heartRate}</td>*/}
-          {/*                    <td>{activity.weight}</td>*/}
-          {/*                </tr>*/}
-          {/*            ))}*/}
-          {/*            </tbody>*/}
-          {/*        </Table>*/}
-          {/*    </div>*/}
-          {/*)}*/}
-
-
+          {activities && renderLineChart}
       </div>
   );
 };
